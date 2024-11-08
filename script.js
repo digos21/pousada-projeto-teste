9c6875af8f995285365af02d79e9475107721a05
digos21 <rodrigo.pereira.it2b@gmail.com>
2024-11-07 22:17:58 -0300

    Novas páginas sendo adicionadas

--- script.js
+++ script.js
@@ -24,3 +24,33 @@ window.onscroll = () => {
         }
     })
 }
+
+// SLIDESOW -----------------------------------------------------------------------------------------
+
+let currentIndex = 0;
+const slides = document.querySelectorAll(".slide");
+
+function updateSlidePosition() {
+  const offset = -currentIndex * 100;
+  document.querySelector(
+    ".slideshow"
+  ).style.transform = `translateX(${offset}%)`;
+}
+
+function changeSlide(direction) {
+  currentIndex += direction;
+
+  // Loop para início ou fim das imagens
+  if (currentIndex < 0) {
+    currentIndex = slides.length - 1;
+  } else if (currentIndex >= slides.length) {
+    currentIndex = 0;
+  }
+
+  updateSlidePosition();
+}
+
+// Atualiza a posição inicial ao carregar a página
+updateSlidePosition();
+
+//imagem https://static.wixstatic.com/media/d28c03_62aa79344dcb4021b7d005a861825989~mv2.jpg/v1/fill/w_240,h_170,q_85,usm_0.66_1.00_0.01/d28c03_62aa79344dcb4021b7d005a861825989~mv2.jpg
